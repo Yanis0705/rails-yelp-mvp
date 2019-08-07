4ebd5e753868c663d80f19d83fcb8e3f3e21139a
Yanis Chaigneau <yanis.chaigneau4@orange.fr>
2019-08-07 14:30:59 +0200

    Add feature restaurants#create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,9 +12,20 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:index, :show, :new]
+  resources :restaurants, only: [:index, :show, :new, :create]
 end
